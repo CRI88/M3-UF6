b6b4ddc935278775a8c6623240c8d7363f4e3823
CRI88 <98832444+CRI88@users.noreply.github.com>
2025-03-18 16:48:04 +0100

    Add files via upload

---
+++ cri/src/test/java/com/example/cri/CriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
